51a35bf2cc5a0856a23daa9b7cb6740ba2ddde73
Zhijie Gu <guzhijie@gmail.com>
2025-11-07 14:29:20 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6760528828-de739",
+  "appId": "1:438131370267:web:531ec76f6b20bad69a4bbe",
+  "apiKey": "AIzaSyBfsqTYd_xgHpZp16TL6vojUS7Eb_jA5d8",
+  "authDomain": "studio-6760528828-de739.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "438131370267"
+};
